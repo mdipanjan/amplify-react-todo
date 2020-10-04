74df351d3796fa7e222b8c8e239001bd3ed116c7
mdipanjan <mdipanjan33@gmail.com>
2020-10-04 18:02:36 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Todo from "../src/components/Todo";
+import AuthTheme from "../src/config/AuthTheme";
+import { Auth } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
+  const [isLoggedIn, chnageLogin] = useState(false);
+
+  const getCurrentUserstatus = async () => {
+    let userStat = await Auth.currentAuthenticatedUser();
+    // if(userStat.username){}
+    return userStat.username;
+    console.log(userStat.username);
+  };
+
+  const updateLoginStatus = async () => {};
+
+  useEffect(() => {
+    console.log("componentdo munt");
+    getCurrentUserstatus();
+
+    return () => {
+      // cleanup
+    };
+  }, []);
+
   return (
     <div className="App">
       <Todo />
@@ -11,4 +33,4 @@ function App() {
   );
 }
 
-export default withAuthenticator(App, { includeGreetings: true });
+export default withAuthenticator(App, { AuthTheme, includeGreetings: true });
